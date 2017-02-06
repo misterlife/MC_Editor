9bd004f4be8e363c59e9169ac9bd68dbbacb73fb
SFort <SFort@sfort.tech>
2017-02-06 10:42:58 +0100

    init

---
+++ Elemental_DB_Editor/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Elemental_DB_Editor
+{
+    partial class ER_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ER_Form));
+            this.button_Login = new System.Windows.Forms.Button();
+            this.button_submit = new System.Windows.Forms.Button();
+            this.comboBox_Versions = new System.Windows.Forms.ComboBox();
+            this.listBox_Version = new System.Windows.Forms.ListBox();
+            this.listBox_Mods = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button_Login
+            // 
+            this.button_Login.Location = new System.Drawing.Point(12, 12);
+            this.button_Login.Name = "button_Login";
+            this.button_Login.Size = new System.Drawing.Size(130, 25);
+            this.button_Login.TabIndex = 1;
+            this.button_Login.Text = "Login";
+            this.button_Login.UseVisualStyleBackColor = true;
+            this.button_Login.Click += new System.EventHandler(this.button_Login_Click);
+            // 
+            // button_submit
+            // 
+            this.button_submit.Enabled = false;
+            this.button_submit.Location = new System.Drawing.Point(12, 12);
+            this.button_submit.Name = "button_submit";
+            this.button_submit.Size = new System.Drawing.Size(130, 25);
+            this.button_submit.TabIndex = 3;
+            this.button_submit.Text = "Update modlist";
+            this.button_submit.UseVisualStyleBackColor = true;
+            this.button_submit.Visible = false;
+            this.button_submit.Click += new System.EventHandler(this.button_submit_Click);
+            // 
+            // comboBox_Versions
+            // 
+            this.comboBox_Versions.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_Versions.Enabled = false;
+            this.comboBox_Versions.FormattingEnabled = true;
+            this.comboBox_Versions.Location = new System.Drawing.Point(148, 15);
+            this.comboBox_Versions.Name = "comboBox_Versions";
+            this.comboBox_Versions.Size = new System.Drawing.Size(130, 21);
+            this.comboBox_Versions.TabIndex = 4;
+            this.comboBox_Versions.SelectedValueChanged += new System.EventHandler(this.comboBox_Versions_SelectedValueChanged);
+            // 
+            // listBox_Version
+            // 
+            this.listBox_Version.Enabled = false;
+            this.listBox_Version.FormattingEnabled = true;
+            this.listBox_Version.Location = new System.Drawing.Point(148, 43);
+            this.listBox_Version.Name = "listBox_Version";
+            this.listBox_Version.Size = new System.Drawing.Size(130, 212);
+            this.listBox_Version.TabIndex = 5;
+            this.listBox_Version.DoubleClick += new System.EventHandler(this.listBox_Version_DoubleClick);
+            // 
+            // listBox_Mods
+            // 
+            this.listBox_Mods.Enabled = false;
+            this.listBox_Mods.FormattingEnabled = true;
+            this.listBox_Mods.Location = new System.Drawing.Point(12, 43);
+            this.listBox_Mods.Name = "listBox_Mods";
+            this.listBox_Mods.Size = new System.Drawing.Size(130, 212);
+            this.listBox_Mods.TabIndex = 6;
+            this.listBox_Mods.DoubleClick += new System.EventHandler(this.listBox_Mods_DoubleClick);
+            // 
+            // ER_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.listBox_Mods);
+            this.Controls.Add(this.listBox_Version);
+            this.Controls.Add(this.comboBox_Versions);
+            this.Controls.Add(this.button_submit);
+            this.Controls.Add(this.button_Login);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(300, 300);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "ER_Form";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ERealms Editor";
+            this.Load += new System.EventHandler(this.ER_Form_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button_Login;
+        private System.Windows.Forms.Button button_submit;
+        private System.Windows.Forms.ComboBox comboBox_Versions;
+        private System.Windows.Forms.ListBox listBox_Version;
+        private System.Windows.Forms.ListBox listBox_Mods;
+    }
+}
+
---
+++ Elemental_DB_Editor/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Elemental_DB_Editor
+{
+    public partial class ER_Form : Form
+    {
+        public string ERConnectionString, ERserver= "51.255.41.80";
+        public string[] SList_Mods;
+        public ER_Form()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ER_Form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Login_Click(object sender, EventArgs e)
+        {
+            button_Login.Enabled = false;
+           string[] login= (Microsoft.VisualBasic.Interaction.InputBox("Username,Password:", "ERealms Connection", "Username,password")).Split(",".ToCharArray());
+            if (login.Length!=2)
+            {
+                MessageBox.Show("You must use the syntax :\"Username,Password\"", "ERealms user error",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(1);
+            }
+
+            ERConnectionString = "server=" + ERserver + ";uid=" + login[0] + ";" +
+                                    "pwd=" + login[1] + ";database=ElementalRealms_ModdedLauncher;";
+
+            MySqlConnection conn = new MySqlConnection(ERConnectionString);
+            string query = "SELECT * FROM ElementalRealms_ModdedLauncher.Version";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            try
+            {
+                conn.Open();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.Write(ex.Message);
+                MessageBox.Show(ex.Message, "ERealms user error",
+                   MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(1);
+            }
+
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+            while (dataReader.Read())
+            {
+                    comboBox_Versions.Items.Add(dataReader["Version_UID"].ToString());
+                    comboBox_Versions.Text = (dataReader["Version_UID"].ToString());
+
+            }
+            comboBox_Versions.Enabled = true;
+            dataReader.Close();
+            conn.Close();
+            button_Login.Enabled = true;
+        }
+
+        private void button_submit_Click(object sender, EventArgs e)
+        {
+            comboBox_Versions.Enabled = false;
+            listBox_Mods.Enabled = false;
+            listBox_Version.Enabled = false;
+            MySqlConnection conn = new MySqlConnection(ERConnectionString);
+            string query = "UPDATE `ElementalRealms_ModdedLauncher`.`Version` SET `Mods`='"+string.Join(",", listBox_Version.Items.Cast<String>().ToList()) + "' WHERE `Version_UID`='" + comboBox_Versions.Text + "'";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            cmd.Connection.Open();
+            cmd.ExecuteNonQuery();
+            conn.CloseAsync();
+            MessageBox.Show("Sucess", "ERealms Feedback",
+                                 MessageBoxButtons.OK, MessageBoxIcon.None);
+        }
+
+        private void listBox_Version_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBox_Version.SelectedItem != null)
+            {
+                string selected = listBox_Version.SelectedItem.ToString();
+                listBox_Mods.Items.Add(selected);
+                listBox_Version.Items.Remove(selected);
+            }
+        }
+
+        private void comboBox_Versions_SelectedValueChanged(object sender, EventArgs e)
+        {
+            button_Login.Visible = false;
+            button_submit.Visible = true;
+            MySqlConnection conn = new MySqlConnection(ERConnectionString);
+            string query = "SELECT * FROM ElementalRealms_ModdedLauncher.Version WHERE Version_UID='" + comboBox_Versions.Text + "'";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+
+
+            try
+            {
+                conn.OpenAsync();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.Write(ex.Message);
+                MessageBox.Show(ex.Message, "ERealms user error",
+                                 MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(1);
+            }
+            listBox_Mods.Items.Clear();
+            listBox_Version.Items.Clear();
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+            while (dataReader.Read())
+            {
+                SList_Mods = (dataReader["Mods"].ToString()).Split(",".ToCharArray());
+            }
+            listBox_Version.Items.AddRange(SList_Mods);
+            dataReader.Close();
+
+            query = "SELECT * FROM ElementalRealms_ModdedLauncher.Mods";
+            cmd = new MySqlCommand(query, conn);
+            dataReader = cmd.ExecuteReader();
+            while (dataReader.Read())
+            {
+                string mod = dataReader["FileName"].ToString();
+                if(!listBox_Version.Items.Contains(mod))
+                listBox_Mods.Items.Add(mod);
+            }
+
+            button_submit.Enabled = true;
+            listBox_Version.Enabled = true;
+            listBox_Mods.Enabled = true;
+            dataReader.Close();
+            conn.CloseAsync();
+        }
+
+        private void listBox_Mods_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBox_Mods.SelectedItem != null)
+            {
+                string selected = listBox_Mods.SelectedItem.ToString();
+                listBox_Version.Items.Add(selected);
+                listBox_Mods.Items.Remove(selected);
+            }
+        }
+    }
+}
